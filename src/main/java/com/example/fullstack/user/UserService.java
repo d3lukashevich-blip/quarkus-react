394666cd2a26a4654bce94c27b103653b1f3ffdd
d3luk <d3.lukashevich@gmail.com>
2026-06-20 00:38:49 +0200

    Initial commit

---
+++ src/main/java/com/example/fullstack/user/UserService.java
@@ -0,0 +1,58 @@
+package com.example.fullstack.user;
+
+import com.example.fullstack.project.Project;
+import com.example.fullstack.task.Task;
+import io.quarkus.elytron.security.common.BcryptUtil;
+import io.quarkus.hibernate.reactive.panache.common.WithTransaction;
+import io.quarkus.hibernate.reactive.panache.common.runtime.ReactiveTransactional;
+import io.smallrye.mutiny.Uni;
+import jakarta.enterprise.context.ApplicationScoped;
+import org.hibernate.ObjectNotFoundException;
+
+import java.util.List;
+
+@ApplicationScoped
+public class UserService {
+
+    public Uni<User> findById(long id) {
+        return User.<User>findById(id)
+                .onItem()
+                .ifNull()
+                .failWith(() -> new ObjectNotFoundException(id, "User"));
+    }
+
+    public Uni<User> findByName(String name) {
+        return User.find("name", name).firstResult();
+    }
+
+    public Uni<List<User>> list() {
+        return User.listAll();
+    }
+
+    @WithTransaction
+    public Uni<User> create(User user) {
+        user.password = BcryptUtil.bcryptHash(user.password);
+        return user.persistAndFlush();
+    }
+
+    @WithTransaction
+    public Uni<User> update(User user) {
+        return findById(user.id)
+                .chain(_ -> User.getSession())
+                .chain(session -> session.merge(user));
+    }
+
+    @WithTransaction
+    public Uni<Void> delete(long id) {
+        return findById(id)
+                .chain(user -> Uni.combine().all().unis(
+                        Task.delete("user.id", user.id),
+                        Project.delete("user.id", user.id)
+                ).asTuple()
+                        .chain(_ -> user.delete()));
+    }
+
+    public Uni<User> getCurrentUser() {
+        return User.find("order by ID").firstResult();
+    }
+}
